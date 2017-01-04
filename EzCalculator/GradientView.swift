eb2a707f235f9bf05e5dc2cd4c9781f3e53a4f85
iOS Student <iosstudent@techmasters-iMac.local>
2017-01-04 16:58:02 +0700

    first commit

---
+++ EzCalculator/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  EzCalculator
+//
+//  Created by iOS Student on 1/4/17.
+//  Copyright © 2017 tek4fun. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EzCalculator/NumberButton.swift
@@ -0,0 +1,21 @@
+//
+//  NumberButton.swift
+//  EzCalculator
+//
+//  Created by iOS Student on 1/4/17.
+//  Copyright © 2017 tek4fun. All rights reserved.
+//
+
+import UIKit
+
+class NumberButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
